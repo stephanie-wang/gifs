ce296dbba5eca8b5e03331b11f65b0a57618556c
Joshua Blum <joshblum@mit.edu>
2014-07-02 00:01:13 -0700

    changesss

---
+++ pouis/scripts/pouis.js
@@ -0,0 +1,23 @@
+// To enable flash fallback, specify the paths for the flashSWF and flashJS
+Dancer.setOptions({
+  flashJS  : '../submodules/dancer.js/lib/soundmanager2.js',
+  flashSWF : '../submodules/dancer.js/lib/soundmanager2.swf'
+});
+
+var
+  audio  = document.getElementsByTagName('audio')[0],
+  dancer = new Dancer(),
+  kick = dancer.createKick({
+    onKick: function ( mag ) {
+      console.log('Kick!');
+    },
+    offKick: function ( mag ) {
+      console.log('no kick :(');
+    }
+  });
+
+// Let's turn this kick on right away
+kick.on();
+
+dancer.load({ src: 'jaws.mp4'})
+dancer.play();
